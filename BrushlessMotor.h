6baac0b3964fe899ea7492b0e8386a4c230d5be1
OkitaTomoya <special1168@gmail.com>
2020-08-25 18:51:38 +0900

    add pwm shield program

--- BrushlessMotor.h
+++ BrushlessMotor.h
@@ -1,20 +1,21 @@
 #ifndef BrushlessMotor_h
 #define BrushlessMotor_h
 #include <Servo.h>
+// Arduino due pinmapping https://www.arduino.cc/en/Hacking/PinMappingSAM3X
+class BrushlessMotor
+{
+public:
+  BrushlessMotor(int pin);
 
-class BrushlessMotor {
-  public:
-    BrushlessMotor(int pin);
-    
-    void ReadPWM();
-    int GetMotorPin();
-    unsigned int GetPwmInput();
-    
-  private:
-    int _MotorPin;
-    volatile  unsigned long _Up = 0;
-    volatile  unsigned long _Down = 0;
-    volatile unsigned int _pwm_input = 0;
+  void ReadPWM();
+  int GetMotorPin();
+  unsigned int GetPwmInput();
+
+private:
+  int _MotorPin;
+  volatile unsigned long _Up = 0;
+  volatile unsigned long _Down = 0;
+  volatile unsigned int _pwm_input = 0;
 };
 
 #endif
--- readPWM_and_output.ino
+++ readPWM_and_output.ino
@@ -1,16 +1,141 @@
+#define USE_USBCON //dueで通信するときに必要
+#include <Servo.h>
 #include "BrushlessMotor.h"
-BrushlessMotor input_motor = BrushlessMotor(35);
+
+// -----adafruit_pwm_servo_driver--------
+#include <Wire.h>
+#include "my_Adafruit_PWMServoDriver.h"
+#define SERVO_FREQ 500
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+//------ros-------
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float32.h>
+ros::NodeHandle nh;
+std_msgs::Float32MultiArray array_msg;
+ros::Publisher pub("array_pub", &array_msg);
+
+//------input motor setting------
+const int m1_input_pin = 28;
+const int m2_input_pin = 36;
+const int m3_input_pin = 44;
+const int m4_input_pin = 52;
+
+BrushlessMotor input_motor1 = BrushlessMotor(m1_input_pin);
+BrushlessMotor input_motor2 = BrushlessMotor(m2_input_pin);
+BrushlessMotor input_motor3 = BrushlessMotor(m3_input_pin);
+BrushlessMotor input_motor4 = BrushlessMotor(m4_input_pin);
+
+//------output motor setting------
+const int m1_output_shieldpin = 0;
+const int m2_output_shieldpin = 1;
+const int m3_output_shieldpin = 2;
+const int m4_output_shieldpin = 3;
+
+int brushless1_command;
+int brushless2_command;
+int brushless3_command;
+int brushless4_command;
+
+// //lateral rotor
+// Servo lateral1_brushlessmotor;
+// Servo lateral2_brushlessmotor;
+// //lateral Servo
+// Servo lateral1_servo1;
+// Servo lateral1_servo2;
+// Servo lateral1_servo3;
+// Servo lateral2_servo1;
+// Servo lateral2_servo2;
+// Servo lateral2_servo3;
+// int lateral1_servo_command;
+// int lateral1_servo1_command;
+// int lateral1_servo2_command;
+// int lateral1_servo3_command;
+// int lateral2_servo_command;
+// int lateral2_servo1_command;
+// int lateral2_servo2_command;
+// int lateral2_servo3_command;
+
+void yawCallback(const std_msgs::Float32MultiArray &command_value)
+{
+    brushless1_command = input_motor1.GetPwmInput();
+    brushless2_command = input_motor2.GetPwmInput();
+    brushless3_command = input_motor3.GetPwmInput();
+    brushless4_command = input_motor4.GetPwmInput();
+
+    // output motor
+    pwm.writeMicroseconds(m1_output_shieldpin, brushless1_command);
+    pwm.writeMicroseconds(m2_output_shieldpin, brushless2_command);
+    pwm.writeMicroseconds(m3_output_shieldpin, brushless3_command);
+    pwm.writeMicroseconds(m4_output_shieldpin, brushless4_command);
+    nh.loginfo(String(brushless1_command).c_str());
+}
+
+ros::Subscriber<std_msgs::Float32MultiArray> sub("yaw_command", &yawCallback);
+
 void setup()
 {
-    Serial.begin(115200);
-    attachInterrupt(input_motor.GetMotorPin(), SUB_input_motor, CHANGE);
+    //-----ros------
+    array_msg.data_length = 12;
+    array_msg.data = (float *)malloc(sizeof(float) * 12);
+
+    nh.getHardware()->setBaud(115200);
+    nh.initNode();
+    nh.advertise(pub);
+
+    //   ---PWMshield--------
+    pwm.begin();
+    pwm.setOscillatorFrequency(25000000);
+    pwm.setPWMFreq(SERVO_FREQ);
+    pwm.writeMicroseconds(m1_output_shieldpin, 1000);
+    pwm.writeMicroseconds(m2_output_shieldpin, 1000);
+    pwm.writeMicroseconds(m3_output_shieldpin, 1000);
+    pwm.writeMicroseconds(m4_output_shieldpin, 1000);
+
+    //-----割り込み------
+    attachInterrupt(input_motor1.GetMotorPin(), SUB_input_motor1, CHANGE);
+    attachInterrupt(input_motor2.GetMotorPin(), SUB_input_motor2, CHANGE);
+    attachInterrupt(input_motor3.GetMotorPin(), SUB_input_motor3, CHANGE);
+    attachInterrupt(input_motor4.GetMotorPin(), SUB_input_motor4, CHANGE);
 }
+
 void loop()
 {
-    Serial.println(input_motor.GetPwmInput());
-    delay(100);
+    if (!nh.connected())
+    { //-----------------------------------------------
+        brushless1_command = 1000;
+        brushless2_command = 1000;
+        brushless3_command = 1000;
+        brushless4_command = 1000;
+        pwm.writeMicroseconds(m1_input_pin, brushless1_command);
+        pwm.writeMicroseconds(m2_input_pin, brushless2_command);
+        pwm.writeMicroseconds(m3_input_pin, brushless3_command);
+        pwm.writeMicroseconds(m4_input_pin, brushless4_command);
+    }
+    else
+    {
+        // pub_data.data[0] = analogRead(A0);
+        // arduino_data.publish(&pub_data);
+    }
+    nh.spinOnce();
+    delayMicroseconds(500);
 }
-void SUB_input_motor()
+
+//-----割り込みサブルーチン------
+void SUB_input_motor1()
 {
-    input_motor.ReadPWM();
+    input_motor1.ReadPWM();
+}
+void SUB_input_motor2()
+{
+    input_motor2.ReadPWM();
+}
+void SUB_input_motor3()
+{
+    input_motor3.ReadPWM();
+}
+void SUB_input_motor4()
+{
+    input_motor4.ReadPWM();
 }
