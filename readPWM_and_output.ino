cf7d9a8526290542a8b5811947120329224fcd01
OkitaTomoya <special1168@gmail.com>
2020-12-16 18:02:50 +0900

    201216 from stickPC

--- readPWM_and_output.ino
+++ readPWM_and_output.ino
@@ -1,4 +1,4 @@
-#define USE_USBCON //dueで通信するときに必要
+#define USE_USBCON //dueÅÊM·éÆ«ÉKv
 #include "Servo.h"
 #include "BrushlessMotor.h"
 
@@ -59,78 +59,75 @@ const int lateral2_servo3_shieldpin = 11;
 float lateral1_force = 0.0;
 float lateral2_force = 0.0;
 
-float roll_d_gain = 0.0; //5.0;
+float roll_d_gain = 18.0; //18.0;
 float roll_velocity = 0.0;
 
 //servo_command=lateral1_thrust_to_servo_command(thrust)--------------------------------------
-const float lateral1_param1[4] = {1.42000573e+03, -1.27692693e+01, -4.62346562e-01, -2.79686070e-02};
-const float lateral1_param2[4] = {1410.018664, -47.59048111, -7.52998927, 24.85048449};
-const float lateral1_param3[4] = {1391.83293, -14.2056781, 0.863936724, -0.0510286803};
+const float lateral1_param1[4] = {1431.1181858358468, -11.132199571700767, -0.35918663681241014, -0.016709300979755426};
+const float lateral1_param2[4] = {1424.1623587070135, -38.13521287160256, -17.562850390397216, 18.769075024676567};
+const float lateral1_param3[4] = {1397.8641637953551, -11.269289655814646, 0.3962092075307737, -0.016555344338769373};
 int lateral1_thrust_to_servo_command(float thrust)
 {
     int command;
+    //limitter
+    if (thrust > 15.0)
+    {
+        thrust = 15.0;
+    }
+    else if (thrust < -15.0)
+    {
+        thrust = -15.0;
+    }
     float x1 = thrust;
     float x2 = thrust * x1;
     float x3 = thrust * x2;
-    if (thrust < -0.7448)
+    if (thrust <= -0.45126)
     {
         command = int(lateral1_param1[0] + lateral1_param1[1] * x1 + lateral1_param1[2] * x2 + lateral1_param1[3] * x3);
     }
-    else if (-0.7448 <= thrust && thrust <= 0.882)
+    else if (-0.45126 < thrust && thrust <= 1.0791)
     {
         command = int(lateral1_param2[0] + lateral1_param2[1] * x1 + lateral1_param2[2] * x2 + lateral1_param2[3] * x3);
     }
-    else if (0.882 < thrust)
+    else if (1.0791 < thrust)
     {
         command = int(lateral1_param3[0] + lateral1_param3[1] * x1 + lateral1_param3[2] * x2 + lateral1_param3[3] * x3);
     }
-
-    //limitter
-    if (command > 1550)
-    {
-        command = 1550;
-    }
-    else if (command < 1230)
-    {
-        command = 1230;
-    }
     return command;
 }
-
-const float lateral2_param1[4] = {1.33485718e+03, -1.26789886e+01, -5.94512376e-01, -3.30347612e-02};
-const float lateral2_param2[4] = {1326.84944302, -62.67557326, 7.02881259, 106.85549092};
-const float lateral2_param3[4] = {1.31021169e+03, -1.40550417e+01, 6.30551538e-01, -3.50974486e-02};
+const float lateral2_param1[4] = {1332.647258536379, -11.011548736056284, -0.5290815914172526, -0.0191764264997118};
+const float lateral2_param2[4] = {1330.5815949288717, -35.44922932778718, -60.37887213536305, 76.42089875279635};
+const float lateral2_param3[4] = {1309.757174467697, -10.841931780203595, 0.4358451117468576, -0.0140146922403801};
 int lateral2_thrust_to_servo_command(float thrust)
 {
     int command;
+    //limitter
+    if (thrust > 15.0)
+    {
+        thrust = 15.0;
+    }
+    else if (thrust < -15.0)
+    {
+        thrust = -15.0;
+    }
     float x1 = thrust;
     float x2 = thrust * x1;
     float x3 = thrust * x2;
-    if (thrust < -0.668)
+    if (thrust <= -0.15696)
     {
         command = int(lateral2_param1[0] + lateral2_param1[1] * x1 + lateral2_param1[2] * x2 + lateral2_param1[3] * x3);
     }
-    else if (-0.668 <= thrust && thrust <= 0.393)
+    else if (-0.15696 < thrust && thrust <= 0.6867)
     {
         command = int(lateral2_param2[0] + lateral2_param2[1] * x1 + lateral2_param2[2] * x2 + lateral2_param2[3] * x3);
     }
-    else if (0.393 < thrust)
+    else if (0.6867 < thrust)
     {
         command = int(lateral2_param3[0] + lateral2_param3[1] * x1 + lateral2_param3[2] * x2 + lateral2_param3[3] * x3);
     }
-
-    //limmitter
-    if (command > 1450)
-    {
-        command = 1450;
-    }
-    else if (command < 1180)
-    {
-        command = 1180;
-    }
     return command;
 }
-
+//------------------------------------------------------
 // ブラシレスモーターの推力からコマンドに変換する関数
 float brushless_force_to_command(float force)
 {
@@ -150,6 +147,7 @@ enum flight_mode
 
 int input_pwm[4];
 int transform_pwm[4];
+float calc_input_pwm[4];
 bool output_motor()
 {
     if (millis() - last_update_time > 500)
@@ -174,10 +172,14 @@ bool output_motor()
     input_pwm[2] = (int)((float)input_motor3.GetPwmInput() - roll_d_gain * roll_velocity);
     input_pwm[3] = (int)((float)input_motor4.GetPwmInput() + roll_d_gain * roll_velocity);
     //yaw操作量を打ち消したもの
-    transform_pwm[0] = (int)(0.25 * (3.0 * input_pwm[0] + input_pwm[1] - input_pwm[2] + input_pwm[3]));
-    transform_pwm[1] = (int)(0.25 * (input_pwm[0] + 3.0 * input_pwm[1] + input_pwm[2] - input_pwm[3]));
-    transform_pwm[2] = (int)(0.25 * (-input_pwm[0] + input_pwm[1] + 3.0 * input_pwm[2] + input_pwm[3]));
-    transform_pwm[3] = (int)(0.25 * (input_pwm[0] - input_pwm[1] + input_pwm[2] + 3.0 * input_pwm[3]));
+    calc_input_pwm[0] = (float)input_motor1.GetPwmInput() + roll_d_gain * roll_velocity;
+    calc_input_pwm[1] = (float)input_motor2.GetPwmInput() + roll_d_gain * roll_velocity;
+    calc_input_pwm[2] = (float)input_motor3.GetPwmInput() + roll_d_gain * roll_velocity;
+    calc_input_pwm[3] = (float)input_motor4.GetPwmInput() + roll_d_gain * roll_velocity;
+    transform_pwm[0] = (int)(0.25 * (3.0 * calc_input_pwm[0] + calc_input_pwm[1] - calc_input_pwm[2] + calc_input_pwm[3]));
+    transform_pwm[1] = (int)(0.25 * (calc_input_pwm[0] + 3.0 * calc_input_pwm[1] + calc_input_pwm[2] - calc_input_pwm[3]));
+    transform_pwm[2] = (int)(0.25 * (-calc_input_pwm[0] + calc_input_pwm[1] + 3.0 * calc_input_pwm[2] + calc_input_pwm[3]));
+    transform_pwm[3] = (int)(0.25 * (calc_input_pwm[0] - calc_input_pwm[1] + calc_input_pwm[2] + 3.0 * calc_input_pwm[3]));
     if (mode == without_lateral) //水平方向ロータを使わないとき
     {
         //DJIよみとった値をそのまま出力
@@ -186,9 +188,9 @@ bool output_motor()
         brushless3_command = input_pwm[2];
         brushless4_command = input_pwm[3];
     }
-    else if ((mode == with_lateral_yaw) || (mode == with_lateral_x_yaw)) //水平方向ロータを使うとき
+    else if ((mode == with_lateral_yaw) || (mode == with_lateral_x_yaw)) //æ°´å¹³æ¹åã­ã¼ã¿ãä½¿ãã¨ã
     {
-        //鉛直方向ロータ：yaw操作量をなくしたものを出力
+    //鉛直方向ロータ：yaw操作量をなくしたものを出力
         brushless1_command = transform_pwm[0];
         brushless2_command = transform_pwm[1];
         brushless3_command = transform_pwm[2];
@@ -203,7 +205,7 @@ bool output_motor()
             int lateral1_servo2_command = lateral1_servo_command;
             int lateral1_servo3_command = lateral1_servo_command;
 
-            //水平方向ロータ２コマンド値計算
+                //水平方向ロータ２コマンド値計算
             int lateral2_servo_command = lateral2_thrust_to_servo_command(lateral2_force);
             int lateral2_servo1_command = 3000 - lateral2_servo_command;
             int lateral2_servo2_command = lateral2_servo_command;
@@ -218,7 +220,7 @@ bool output_motor()
             // brushless4_command += brushless_force_to_command(ff_gain * 0.050 / 0.450 / 4.0 * (lateral1_force + lateral2_force));
             // //=============================================================================================================================================
 
-            //水平方向ロータ出力
+              //水平方向ロータ出力
             pwm.writeMicroseconds(lateral1_brushless_shiledpin, 1800);
             pwm.writeMicroseconds(lateral2_brushless_shiledpin, 1800);
             pwm.writeMicroseconds(lateral1_servo1_shieldpin, lateral1_servo1_command);
@@ -253,8 +255,8 @@ bool output_motor()
             pwm.writeMicroseconds(lateral2_servo3_shieldpin, lateral2_servo3_command);
         }
     }
-    m1.writeMicroseconds(brushless1_command);
     m2.writeMicroseconds(brushless2_command);
+    m1.writeMicroseconds(brushless1_command);
     m3.writeMicroseconds(brushless3_command);
     m4.writeMicroseconds(brushless4_command);
     return true;
@@ -279,7 +281,6 @@ void arduinoCallback(const std_msgs::Float32MultiArray &command_value)
     {
         lateral1_force = u_x + u_yaw;
         lateral2_force = u_x - u_yaw;
-        nh.logwarn(String(lateral1_force).c_str());
     }
     // nh.logwarn(String(brushless1_command).c_str());
 }
@@ -287,7 +288,7 @@ void arduinoCallback(const std_msgs::Float32MultiArray &command_value)
 void angularVelocityCallback(const geometry_msgs::Vector3Stamped &angular_velocity)
 {
     roll_velocity = angular_velocity.vector.x;
-    nh.logwarn(String(roll_d_gain * roll_velocity).c_str());
+    // nh.logwarn(String(roll_d_gain * roll_velocity).c_str());
 }
 ros::Subscriber<std_msgs::Float32MultiArray> sub1("arduino_control", &arduinoCallback);
 ros::Subscriber<geometry_msgs::Vector3Stamped> sub2("/dji_sdk/angular_velocity_fused", &angularVelocityCallback);
@@ -346,7 +347,10 @@ void loop()
         array_msg.data[2] = input_motor3.GetPwmInput();
         array_msg.data[3] = input_motor4.GetPwmInput();
         success_output = output_motor();
-        // nh.logwarn(String(success_output).c_str());
+        // nh.logwarn(String(array_msg.data[0]).c_str());
+        //  nh.logwarn(String(array_msg.data[1]).c_str());
+        //   nh.logwarn(String(array_msg.data[2]).c_str());
+        //    nh.logwarn(String(array_msg.data[3]).c_str());
         arduino_data.publish(&array_msg);
         nh.spinOnce();
     }
